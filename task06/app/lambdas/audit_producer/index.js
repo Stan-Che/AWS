82ffdc91a258712893d41fc52dc11ce1b7e09634
Stan Che <stanislav.chernetskyi@kingfisher.com>
2024-06-21 17:01:08 +0200

    add task06

---
+++ task06/app/lambdas/audit_producer/index.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const eventNames = {
+    INSERT: 'INSERT',
+    MODIFY: 'MODIFY',
+};
+
+const constructItem = (eventName, key, newValue, oldValue) => {
+    return {
+        id: uuidv4(),
+        itemKey: key,
+        modificationTime: new Date().toISOString(),
+
+        ...(eventName === eventNames.INSERT 
+            ? { newValue: { key, value: newValue }}
+            : { updatedAttribute: "value", oldValue, newValue }
+        )
+    }
+}
+
+exports.handler = async (event) => {
+    console.log('event', event);
+    console.log('event Records', event.Records);
+
+    for (const record of event.Records) {
+        const { eventName, dynamodb: { NewImage = {}, OldImage = {} } } = record;
+        const { value: newValue, key } = NewImage;
+        const { value: oldValue = {} } = OldImage;
+        // console.log('dynamodb content', dynamodb);
+        // console.log(`key ${key.S} | new value ${newValue.N}`);
+        // console.log(`key ${key.S} | old value ${oldValue.N}`);
+        
+        const params = {
+            TableName:  process.env.audit_table,
+            Item: constructItem(eventName, key.S, newValue.N, oldValue.N),
+        };
+        
+        console.log('params', params);
+        // console.log('process.env.target_table', process.env.audit_table);
+        try {
+            await dynamoDb.put(params).promise();
+            console.log('dynamodb put succeeded');
+        } catch (err) {
+            console.log('dynamodb put failed', err);
+        }
+    }
+};
